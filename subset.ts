b07a1c88fb2e04f60799c43cb1f0c8b95777c628
Kevin Rasmusson <kevin.rasmusson@hotmail.com>
2022-09-26 21:47:03 -0700

    subset getter working

--- subset.ts
+++ subset.ts
@@ -1,15 +1,15 @@
-let S: Set<number> = new Set<number>([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+let S: Set<number> = new Set<number>([1, 2, 3, 4]);
 let setOfSubsets: Set<Set<number>> = new Set<Set<number>>();
 let numberOfSubsets: number = Math.pow(2, S.size);
 
 for (let i = 0; i < numberOfSubsets; i++) {
-    // let subset: Set<number> = new Set<number>();
-    // let j: number = 0;
-    // for (let element of S) {
-    //     if ((i & (1 << j)) > 0) {
-    //         subset.add(element);
-    //     }
-    //     j++;
-    // }
-    // setOfSubsets.add(subset);
+    let subset: Set<number> = new Set<number>();
+    let index: number = i;
+    for (let k: number = 0; k < S.size; k++) {
+        if (index & 0b1) subset.add(k+1);
+        index = index >> 1;
+    }
+    setOfSubsets.add(subset);
 }
+
+console.log(setOfSubsets);
